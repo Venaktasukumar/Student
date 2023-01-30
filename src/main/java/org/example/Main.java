19f63f3b460d66248962c78f8acffc60d20e90b4
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-01-30 18:56:22 +0530

    COMMIT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,5 @@
 package org.example;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.*;
 class Student {
     Logger l = Logger.getLogger("com.api.jar");
@@ -21,30 +21,39 @@ class Student {
     public static void main(String[] args){
         Logger l = Logger.getLogger("com.api.jar");
         Scanner sc=new Scanner(System.in);
-        l.info("Enter the student name:");
-        String studentname=sc.nextLine();
-        l.info("Enter your Grade:");
-        char gradelevel=sc.next().charAt(0);
-        l.info("Enter your GPA:");
-        float gpa=sc.nextFloat();
-        Student s=new Student(studentname,gradelevel,gpa);
-        while(true){
-            l.info("1.Upadte");
-            l.info("2.Display");
-            l.info("3.Exit");
-            int a= sc.nextInt();
-            if(a==1){
-                l.info("Enter your GPA");
-                float gPA=sc.nextFloat();
-                s.updateGPA(gPA);
+        try{
+            l.info("Enter the student name:");
+            String studentname=sc.nextLine();
+            l.info("Enter your Grade:");
+            char gradelevel=sc.next().charAt(0);
+            l.info("Enter your GPA:");
+            float gpa=sc.nextFloat();
+            Student s=new Student(studentname,gradelevel,gpa);
+            while(true){
+                l.info("1.Upadte");
+                l.info("2.Display");
+                l.info("3.Exit");
+                int a= sc.nextInt();
+                if(a==1){
+                    l.info("Enter your GPA");
+                    float gPA=sc.nextFloat();
+                    s.updateGPA(gPA);
+                }
+                else if(a==2){
+                    String z=s.displayGPa();
+                    l.info(z);
+                }
+                else if(a==3){
+                    l.info("Exit");
+                    break;
+                }
+                else{
+                    l.info("invalid choice");
+                }
             }
-            else if(a==2){
-                String z=s.displayGPa();
-                l.info(z);
-            }
-            else{
-                break;
-            }
+        }
+        catch(InputMismatchException e){
+            l.info("invalid input "+e);
         }
         sc.close();
     }
